22ec114a37b6b1beefa76c978489e72630eb1a40
Derek Kilgore <98043070+DKILGORE79@users.noreply.github.com>
2022-07-02 13:37:32 -0500

    reuploaded to new repo

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,10 @@
 
 // Start working code
-// User input variables: 
 var enter;
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
-// Start Password variable values: 
 // Special characters 
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 // Numeric characters
@@ -21,7 +19,7 @@ var choices;
 var toUpper = function (x) {
   return x.toUpperCase();
 };
-// creates a variable for uppercase conversion
+// creates a var for uppercase
 alpha2 = alpha.map(toUpper);
 
 var get = document.querySelector("#generate");
@@ -34,12 +32,12 @@ get.addEventListener("click", function () {
 // Start function to generate password
 function generatePassword() {
   // Asks for user input
-  enter = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+  enter = parseInt(prompt("How many characters would you like your password to be? Choose between 8 and 128"));
   // First if statement for user validation 
   if (!enter) {
-    alert("This needs a value");
+    alert("Please enter a value");
   } else if (enter < 8 || enter > 128) {
-    // Validates user input
+
     // Start user input prompts
     enter = parseInt(prompt("You must choose between 8 and 128"));
 
@@ -56,7 +54,7 @@ function generatePassword() {
     choices = alert("You must choose a criteria!");
 
   }
-  // First if statement that uses user input prompts to determine choices
+  //  The start of the If/then statements
   // Else if for 4 positive options
   else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
 
@@ -112,32 +110,20 @@ function generatePassword() {
   // password variable is an array placeholder for user generated amount of length
   var password = [];
 
-  // Start random selection variables:
+
   // Random selection for all variables: 
   for (var i = 0; i < enter; i++) {
     var pickChoices = choices[Math.floor(Math.random() * choices.length)];
     password.push(pickChoices);
   }
   // This joins the password array and converts it to a string
-  // Worked with a tutor to incorporate this option
   var ps = password.join("");
   UserInput(ps);
   return ps;
 }
-// This puts the password value into the textbox
+
 // Changed function input to use textcontent
 function UserInput(ps) {
   document.getElementById("password").textContent = ps;
 
-}
-
-var copy = document.querySelector("#copy");
-copy.addEventListener("click", function () {
-  copyPassword();
-});
-// This copies the password value - works
-function copyPassword() {
-  document.getElementById("password").select();
-  document.execCommand("Copy");
-  alert("Password copied to clipboard!");
 }
